8f263a0d875d20499e5f78fc85b6abd5334ecbec
Long Phan <100278075+Longphan2810@users.noreply.github.com>
2024-09-07 16:20:00 +0700

    update mapStruct

--- src/main/java/com/example/demo/controller/FirstTestController.java
+++ src/main/java/com/example/demo/controller/FirstTestController.java
@@ -5,7 +5,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.entity.FirstTestDb;
 import com.example.demo.respone.ApiRespone;
+import com.example.demo.respone.FirstTestResponeDTO;
 import com.example.demo.service.impl.FirstTestServiceImpl;
+
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,11 +24,15 @@ public class FirstTestController {
 	FirstTestServiceImpl firstTestServiceImpl;
 	
 	@GetMapping("firstTest/{id}")	
-	public ApiRespone<FirstTestDb> getFirstTest(@PathVariable("id")  int id){
-		ApiRespone<FirstTestDb> response = new ApiRespone<FirstTestDb>();
-		
-		response.setResult(firstTestServiceImpl.getById(id));
+	public ApiRespone<FirstTestResponeDTO> getFirstTest(@PathVariable("id")  int id){
+		ApiRespone<FirstTestResponeDTO> response = new ApiRespone<FirstTestResponeDTO>();
+		response.setResult(firstTestServiceImpl.getFirstTestResponeDTOById(id));
 		return response;
 	}
-	
+	@GetMapping("firstTests")	
+	public ApiRespone<List<FirstTestResponeDTO>> getFirstTests(){
+		ApiRespone<List<FirstTestResponeDTO>> response = new ApiRespone<List<FirstTestResponeDTO>>();
+		response.setResult(firstTestServiceImpl.findAll());
+		return response;
+	}
 }
--- src/main/java/com/example/demo/service/impl/FirstTestServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/FirstTestServiceImpl.java
@@ -1,10 +1,14 @@
 package com.example.demo.service.impl;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.demo.entity.FirstTestDb;
+import com.example.demo.map.FirstTestMapper;
 import com.example.demo.repository.FirstTestDbRepository;
+import com.example.demo.respone.FirstTestResponeDTO;
 
 @Service
 public class FirstTestServiceImpl {
@@ -12,9 +16,27 @@ public class FirstTestServiceImpl {
 	@Autowired
 	FirstTestDbRepository firstTestDbRepository;
 	
+	@Autowired
+	FirstTestMapper firstTestMapper;
+	
 	public FirstTestDb getById(int id) {
 		
 		return firstTestDbRepository.findById(id).orElseThrow(()-> new RuntimeException("NULL_FT"));
 	}
+	public FirstTestResponeDTO getFirstTestResponeDTOById(int id) {
+		
+		return firstTestDbRepository.findById(id)
+				.map(e -> firstTestMapper.toFirstTestResponeDTO(e))
+				.orElseThrow(()-> new RuntimeException("NULL_FT"));
+	}
+	public List<FirstTestResponeDTO> findAll() {
+		return firstTestDbRepository.findAll()
+				.stream()
+				.map(e -> firstTestMapper.toFirstTestResponeDTO(e))
+				.toList();
+	}
+	
+	
+	
 	
 }
