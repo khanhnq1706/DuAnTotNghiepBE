9d8d1b98e5d6a6e04eaba340becf69b6c84af120
khanhnqpd1706 <khanhzalo321@gmail.com>
2024-09-21 01:57:25 +0700

    upadte db order

--- src/main/java/com/example/demo/entity/OrderDetailEntity.java
+++ src/main/java/com/example/demo/entity/OrderDetailEntity.java
@@ -1,6 +1,12 @@
 package com.example.demo.entity;
 
+import com.example.demo.enums.PaymentMethod;
+import com.example.demo.enums.PaymentStatus;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -19,7 +25,18 @@ import lombok.experimental.FieldDefaults;
 public class OrderDetailEntity  {
 	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
 	int idOrderDetail;
-	
+	@Column(nullable = false)
+	int quantity;
+	@Column(nullable = false)
+	long price;
+	@Column(nullable = false)
+	long totalPrice;
+	@Enumerated(EnumType.STRING)
+	PaymentStatus paymentStatus;
+	@Enumerated(EnumType.STRING)
+	PaymentMethod paymentMethod;
+	@Column(columnDefinition = "nvarchar(255)")
+	String note;
 	@ManyToOne
 	@JoinColumn(name = "id_order")
 	OrderEntity orderEntity;
--- src/main/java/com/example/demo/entity/OrderEntity.java
+++ src/main/java/com/example/demo/entity/OrderEntity.java
@@ -1,8 +1,14 @@
 package com.example.demo.entity;
 
+import java.util.Date;
 import java.util.List;
 
+import com.example.demo.enums.OrderStatus;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -10,6 +16,8 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -25,18 +33,22 @@ public class OrderEntity extends BaseEntity {
 	@Id 
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	int idOrder;
-	
-	String statusOrder;
-	
+	 @Enumerated(EnumType.STRING)
+	OrderStatus statusOrder;
+	 @Temporal(TemporalType.DATE)
+	Date createdDate;
+	 @Temporal(TemporalType.DATE)
+	Date updatedDate;
 	@OneToOne
 	@JoinColumn(name = "id_table")
 	TableEntity tableEntity;
-	
 	@ManyToOne
 	@JoinColumn(name = "id_User")
 	UserEnitty userEnitty;
 	
 	@OneToMany(mappedBy = "orderEntity")
 	List<OrderDetailEntity> listOrderDetail;
-	
+	public void preCreate() {
+		createdDate = new Date();
+	}
 }
