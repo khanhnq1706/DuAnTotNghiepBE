858bf8119e1f13d726238f628d3f5eecbbf5a74f
Minh200463 <kuidndjj@gmail.com>
2024-09-23 17:57:21 +0700

    ok

--- src/main/java/com/example/demo/service/TableService.java
+++ src/main/java/com/example/demo/service/TableService.java
@@ -1,15 +1,6 @@
 package com.example.demo.service;
 
 import java.util.List;
-import java.util.stream.Collectors;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Service;
-
-import com.example.demo.entity.TableEntity;
-import com.example.demo.map.TableMapper;
-import com.example.demo.repository.TableRepository;
 import com.example.demo.request.TableRequestDTO;
 import com.example.demo.respone.ApiRespone;
 import com.example.demo.respone.TableResponseDTO;
