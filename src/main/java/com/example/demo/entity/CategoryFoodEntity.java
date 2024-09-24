ad74678a4ec50faac59751480b6a662119e6350d
tranducluong <tranducluong886@gmail.com>
2024-09-24 12:57:14 +0700

    Lưu các thay đổi của CategoryFoodEntity trước khi pull

--- src/main/java/com/example/demo/entity/CategoryFoodEntity.java
+++ src/main/java/com/example/demo/entity/CategoryFoodEntity.java
@@ -2,14 +2,13 @@ package com.example.demo.entity;
 
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
@@ -18,17 +17,20 @@ import lombok.NoArgsConstructor;
 import lombok.experimental.FieldDefaults;
 
 @Data
-@NoArgsConstructor @AllArgsConstructor
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class CategoryFoodEntity extends BaseEntity {
-	@Id  @GeneratedValue(strategy = GenerationType.IDENTITY)
-	int idCategory;
-	boolean isDeleted;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    int idCategory;
+    boolean isDeleted;
+	
+    @Column(columnDefinition = "varchar(100)")
+    String nameCategory;
 
-	@Column(columnDefinition = "varchar(100)")
-	String nameCategory;
-
-	@OneToMany(mappedBy = "category")
-	List<FoodEntity> listFoodCreated;
+    @OneToMany(mappedBy = "category")
+    @JsonIgnore
+    List<FoodEntity> listFoodCreated;
 }
