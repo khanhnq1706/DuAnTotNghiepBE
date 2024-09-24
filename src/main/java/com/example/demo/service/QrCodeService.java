1c0ea2223186db92c7735f98c9f7609a63253ae3
Minh200463 <kuidndjj@gmail.com>
2024-09-24 22:09:58 +0700

    create feature/15-53

--- src/main/java/com/example/demo/service/QrCodeService.java
+++ src/main/java/com/example/demo/service/QrCodeService.java
@@ -1,5 +1,17 @@
 package com.example.demo.service;
 
-public class QrCodeService {
+import java.util.List;
 
+import com.example.demo.respone.ApiRespone;
+import com.example.demo.respone.QrResposneDTO;
+
+public interface QrCodeService {
+
+    QrResposneDTO createQr(int idTable);
+
+    QrResposneDTO recreateQr(int idTable);
+
+    ApiRespone<QrResposneDTO> getQrcodeByIdTable(int idTable);
+
+    ApiRespone<List<QrResposneDTO>> getAllQrCode();
 }
