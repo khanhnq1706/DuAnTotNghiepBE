5cf7e2a978724035c256da3f3a775a7a984e6eff
Minh200463 <kuidndjj@gmail.com>
2024-09-29 12:14:52 +0700

    done feature/15-53

--- src/main/java/com/example/demo/entity/TableEntity.java
+++ src/main/java/com/example/demo/entity/TableEntity.java
@@ -1,12 +1,16 @@
 package com.example.demo.entity;
 
+import com.example.demo.enums.TableStatus;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToOne;
+import jakarta.validation.constraints.Min;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -27,6 +31,10 @@ public class TableEntity {
 	int idTable;
 	String nameTable;
 	boolean isDeleted;
+	@Min(0)
+	private int maxCapacity;
+	@Enumerated(EnumType.STRING)
+	private TableStatus status;
 
 	@JsonIgnore
 	@OneToOne(mappedBy = "tableEntity")
