4563b6fc08b8485e5976eaace701c9ba0ce5ac5f
Minh200463 <kuidndjj@gmail.com>
2024-10-31 01:53:58 +0700

    ok

--- src/main/java/com/example/demo/config/CorsConfig.java
+++ src/main/java/com/example/demo/config/CorsConfig.java
@@ -6,21 +6,22 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 @Configuration
 public class CorsConfig {
 	@Value("${host.fe}")
-	String hostFE ;
+	String hostFE;
+
 	@Bean
 	public WebMvcConfigurer corsConfigurer() {
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
 				registry.addMapping("/**")
-						.allowedOrigins("**","http://localhost:4200",hostFE)
+						.allowedOrigins("http://localhost:4200", hostFE)
 						.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
 						.allowedHeaders("*")
-						.allowCredentials(true)
-				;
+						.allowCredentials(true);
 			}
 		};
 	}
--- src/main/java/com/example/demo/service/AreaService.java
+++ src/main/java/com/example/demo/service/AreaService.java
@@ -4,7 +4,18 @@ import java.util.List;
 
 import com.example.demo.entity.AreaEntity;
 import com.example.demo.entity.CategoryFoodEntity;
+import com.example.demo.request.AreaRequestDTO;
+import com.example.demo.respone.ApiRespone;
+import com.example.demo.respone.AreaResponeDTO;
 
 public interface AreaService {
     public List<AreaEntity> findAll();
+
+    public AreaResponeDTO createArea(AreaRequestDTO request);
+
+    public ApiRespone<?> deleteArea(int idArea);
+
+    public AreaResponeDTO updateArea(AreaRequestDTO request, int idArea);
+
+    public ApiRespone<AreaResponeDTO> getArea(int idArea);
 }
